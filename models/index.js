95671d99f88f46b61486f07babd8249b7477ade2
Alemneh <Alemneh@hotmail.com>
2016-03-28 09:54:40 -0700

    added session model

---
+++ models/index.js
@@ -0,0 +1,10 @@
+'use strict';
+let mongoose = require('mongoose');
+mongoose.connect(process.en.MONG_URI);
+let models = {};
+
+require('./session')(mongoose, models);
+require('./subject')(mongoose, models);
+require('./table')(mongoose, models);
+
+module.exports = models;
--- models/session.js
+++ models/session.js
@@ -1,1 +1,11 @@
-'use strict'
+'use strict';
+module.exports = (mongoose, models) => {
+  let sessionSchema = mongoose.Schema({
+    timeIn: Date,
+    timeOut: Date,
+    subject: String,
+    table: String
+  });
+  let Session = mongoose.model('Session', sessionSchema);
+  module.Session = Session;
+}
